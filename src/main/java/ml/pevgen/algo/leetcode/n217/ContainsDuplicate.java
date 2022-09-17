f54c4fd91bc6e012581ecf874f2f4ec3dba2a452
pevgen <pevgen@gmail.com>
2022-09-17 11:56:13 +0300

    leetcode 217 (Contains Duplicate) v.2. with sort

--- src/main/java/ml/pevgen/algo/leetcode/n217/ContainsDuplicate.java
+++ src/main/java/ml/pevgen/algo/leetcode/n217/ContainsDuplicate.java
@@ -1,5 +1,6 @@
 package ml.pevgen.algo.leetcode.n217;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -15,6 +16,9 @@ public class ContainsDuplicate {
         System.out.println("in: {1,2,3,4}  (expected: false) = " + ContainsDuplicate.containsDuplicateWithSet(new int[]{1, 2, 3, 4}));
         System.out.println("in: {1,1,1,3,3,4,3,2,4,2}  (expected: true) = " + ContainsDuplicate.containsDuplicateWithSet(new int[]{1, 1, 1, 3, 3, 4, 3, 2, 4, 2}));
 
+        System.out.println("in: {1,2,3,1}  (expected: true) = " + ContainsDuplicate.containsDuplicateWithSort(new int[]{1, 2, 3, 1}));
+        System.out.println("in: {1,2,3,4}  (expected: false) = " + ContainsDuplicate.containsDuplicateWithSort(new int[]{1, 2, 3, 4}));
+        System.out.println("in: {1,1,1,3,3,4,3,2,4,2}  (expected: true) = " + ContainsDuplicate.containsDuplicateWithSort(new int[]{1, 1, 1, 3, 3, 4, 3, 2, 4, 2}));
     }
 
     public static boolean containsDuplicateWithSet(int[] nums) {
@@ -27,4 +31,17 @@ public class ContainsDuplicate {
         }
         return false;
     }
+
+    public static boolean containsDuplicateWithSort(int[] nums) {
+        Arrays.sort(nums);
+        int prevNum = nums[0];
+        for (int i = 1 ; i < nums.length; i++) {
+            if (prevNum == nums[i]) {
+                return true;
+            }
+            prevNum = nums[i];
+        }
+        return false;
+    }
+
 }
