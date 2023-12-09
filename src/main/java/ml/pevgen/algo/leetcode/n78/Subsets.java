5e3173d4d128f46515fd758b989967e6e6b7fc96
pevgen <pevgen@gmail.com>
2023-12-09 13:34:27 +0300

    Leetcode 78. Subsets (v2. backtracking)

--- src/main/java/ml/pevgen/algo/leetcode/n78/Subsets.java
+++ src/main/java/ml/pevgen/algo/leetcode/n78/Subsets.java
@@ -9,7 +9,35 @@ import java.util.List;
  * https://leetcode.com/problems/subsets/description/</a>
  */
 public class Subsets {
+
+
     public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+//        addBranch(nums);
+        backtrack(0, nums, new ArrayList<>(), result);
+        return result;
+    }
+
+    /**
+     *  V 2  - better choice, backtracking solution
+     *  <a href="https://hackernoon.com/backtracking-leetcode-pattern-permutations-vs-subsets-in-java">
+     *      https://hackernoon.com/backtracking-leetcode-pattern-permutations-vs-subsets-in-java</a>
+     */
+    void backtrack(int leftIndex, int[] nums, List<Integer> currentList, List<List<Integer>> resultList) {
+        resultList.add(new ArrayList<>(currentList)); // 'visited' function first
+        for (int i = leftIndex; i < nums.length; i++) {
+            currentList.add(nums[i]);
+            backtrack(i + 1, nums, currentList, resultList);
+            currentList.remove(currentList.size() - 1);
+        }
+    }
+
+
+    /************************************************************
+     *                       V 1
+     ************************************************************/
+
+    public List<List<Integer>> addBranch(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         result.add(Collections.emptyList());
 
